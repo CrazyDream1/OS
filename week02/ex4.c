d60b1b8eb94117ec92de28f8e1c4b57616a233df
CrazyDream1 <35272226+CrazyDream1@users.noreply.github.com>
2019-08-29 18:44:31 +0300

    Add files via upload

---
+++ week02/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void my_swap(int *a, int* b)
+{
+    int t = *b;
+    *b = *a;
+    *a = t;
+}
+
+int main() {
+	printf("Enter two integers:\n");
+	int a, b;
+	scanf("%d %d", &a, &b);
+	my_swap(&a, &b);
+	printf ("After the swap:\n%d - %d\n", a, b);
+}
